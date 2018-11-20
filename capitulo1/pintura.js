3368a0faac2510c9cf42909676248efaf43776f2
manuel_ruiz <rrmanuel1123@gmail.com>
2018-11-20 12:19:06 -0700

    tablero stuff

---
+++ capitulo1/pintura.js
@@ -0,0 +1,17 @@
+function pinta(x,y,color){
+    let colores = [
+        "#FF0000",  //rojo
+        "#FFFF00",  //amarillo
+        "#00FF00",  //verde alien
+        "#0000FF",  //azul
+        "#000000",  //negro
+        "#FFFFFF"   //blanco
+    ] 
+
+    if(!colores.includes(color)){
+        return "Color invalido"
+    }
+    console.log(color)
+    let sqr = document.getElementById(`[${x},${y}]`)
+    sqr.style.backgroundColor = color
+}
